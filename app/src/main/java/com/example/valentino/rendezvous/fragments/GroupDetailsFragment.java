7e8f83a178a37b0cc3da5a65db5bef24c6fe46e8
Aditya Duri <aditya.duri@gmail.com>
2017-11-29 15:05:20 -0600

    Updated Group Details Screen

--- app/src/main/java/com/example/valentino/rendezvous/fragments/GroupDetailsFragment.java
+++ app/src/main/java/com/example/valentino/rendezvous/fragments/GroupDetailsFragment.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.valentino.rendezvous.R;
 import com.example.valentino.rendezvous.models.Event;
@@ -16,6 +19,8 @@ import com.example.valentino.rendezvous.models.Group;
 public class GroupDetailsFragment extends Fragment {
 
     Group group;
+
+    TextView nameField;
 
     public GroupDetailsFragment() {
 	// Required empty public constructor
@@ -28,13 +33,21 @@ public class GroupDetailsFragment extends Fragment {
 	    group = (Group) getArguments().getSerializable("Group");
 	    Toast.makeText(getContext(), group.getName(), Toast.LENGTH_SHORT).show();
 	}
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			     Bundle savedInstanceState) {
 	// Inflate the layout for this fragment
-	return inflater.inflate(R.layout.fragment_group_details, container, false);
+	View view = inflater.inflate(R.layout.fragment_group_details, container, false);
+
+	nameField = (TextView) view.findViewById(R.id.groupNameField);
+	nameField.setText(group.getName());
+
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(group.getName());
+
+        return view;
     }
 
 
